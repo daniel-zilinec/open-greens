0f73e3975e8f5022383b71efbf21f8e58aee2c75
daniel-zilinec <47395846+daniel-zilinec@users.noreply.github.com>
2024-02-23 19:08:28 +0100

    v0.2 - KitchenGreens prototype

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from machine import Pin, PWM
 import time
 
-print("OpenGreens v0.1")
+print("OpenGreens v0.2 - KitchenGreens")
 mg.light(1)
+mg.pump(ON);		# Pump relay disconnects fan power supply
+mg.fan(50);
